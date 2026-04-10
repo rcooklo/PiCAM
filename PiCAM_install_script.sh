ecdcdf186ee2e008d00e02db2c5571f3dcf156c7
rcooklo <rob.c@zombiechicken.com>
2026-04-10 16:18:58 -0700

    Add files via upload

--- PiCAM_install_script.sh
+++ PiCAM_install_script.sh
@@ -2,9 +2,13 @@
 #be sure camera is working (rpicam-hello) before running install 
 sudo apt update
 
-##copy program files including modified sd script
-scp -r rob@bullwinkle:/extra/raspberry/SecurityCamera/files/* .
-
+mv python ../
+mv checknetwork.sh ..
+mv startscript.sh ..
+sudo mv sd /usr/bin/sd
+sudo chmod +x /usr/bin/sd
+cat profileupdate.txt >> ~/.profile
+cd ~
 sudo apt install -y python3-pip python3-venv libcap-dev
 sudo apt install -y python3-picamera2 python3-libcamera
 python -m venv --system-site-packages venv
@@ -15,18 +19,14 @@ pip3 install adafruit-python-shell
 cd python
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo -E env PATH=$PATH python3 raspi-blinka.py
-#source venv/bin/activate
 pip3 install adafruit-circuitpython-pca9685
 pip install adafruit-circuitpython-servokit
 pip install RPi.GPIO
-cd python/
 sudo mknod FIFO_piservo p
 sudo chmod 666 FIFO_piservo
 pip3 install flask
 pip3 install waitress
 cd ~
-sudo mv sd /usr/bin/sd
-sudo chmod +x /usr/bin/sd
 chmod +x startscript.sh 
 chmod +x python/flask/*.sh
 chmod +x /home/pi/python/mjpeg_server.py
@@ -35,7 +35,6 @@ chmod +x /home/pi/python/network_servo.py
 pversion=`ls /usr/bin/python3.??`
 echo $pversion
 sudo setcap CAP_NET_BIND_SERVICE=+eip $pversion
-cat profileupdate.txt >> .profile
 ## possible additions
 #source venv/bin/activate
 #cd python
